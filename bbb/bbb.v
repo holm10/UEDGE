640e83099491a9e068f171933882ddb616efbe29
Andreas Holm <60451789+holm10@users.noreply.github.com>
2023-02-28 13:27:30 -0800

    Rundt bugfix

--- bbb/bbb.v
+++ bbb/bbb.v
@@ -3827,8 +3827,8 @@ yielz(imx+1,lnst+1)    _real
 ***** Ident_vars:
 uedge_ver  character*80 /'$Name: V7_09_02 $'/
 uedge_date character*80 /'Version date in README_Uedge_vers in dir uedge'/
-run_id  integer /0/ # Identifier for use with uetools
-max_run_id  integer /0/ # Identifier for max allocated runs, use with uetools
+session_id  integer /0/ # Identifier for use with uetools
+max_session_id  integer /0/ # Identifier for max allocated runs, use with uetools
 exmain_evals  integer /0/ # Number of successfull exmain evaluations
 
 ***** Last_group_ex_sav_var:
